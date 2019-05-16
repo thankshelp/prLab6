f86cc3027dbbb3a16c0ccc5a95f65f602b7b4a83
Sidsand <dshordg@gmail.com>
2019-05-16 15:18:45 +0700

    mp4

--- MP3/MP3/MainWindow.xaml.cs
+++ MP3/MP3/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Media;
+using System.Windows.Controls.Primitives;
+using System.Security;
 
 namespace MP3
 {
@@ -21,23 +23,57 @@ namespace MP3
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer Timer;
         Dictionary<string, string> list = new Dictionary<string, string>();
         MediaPlayer player = new MediaPlayer();
         TimeSpan ts;
         string nm;
-        Duration time;
-
+        Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+        bool daHotKak = false;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            player.MediaOpened += Player_MediaOpened;
+
+            Timer = new System.Windows.Threading.DispatcherTimer();
+            Timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+               
+
+        }
+
+        //private void InitializeOpenFileDialog()
+        //{
+            
+        //    this.dlg.Filter = "MP3 (*.MP3)|*.MP3";
+        //    this.dlg.Multiselect = true;
+        //    this.dlg.Title = "My Music Browser";
+        //}
+
+        private void Player_MediaOpened(object sender, EventArgs e)
+        {
+            
+            slid.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+            slid.Value = 0;
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (daHotKak == false)
+            {
+                tv.Content = player.Position.Hours + ":" + player.Position.Minutes + ":" + player.Position.Seconds;
+                slid.Value = player.Position.TotalSeconds;
+            }
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
             player.Play();
-            dlina.Content = time;
-            tv.Content = player.Position;
+            Timer.Start();
+            dlina.Content = player.NaturalDuration.TimeSpan.Hours + ":" + player.NaturalDuration.TimeSpan.Minutes + ":" + player.NaturalDuration.TimeSpan.Seconds;
+            tv.Content = "0:0:0";
         }
 
         private void Stack_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -45,44 +81,88 @@ namespace MP3
             nm = stack.Items[stack.SelectedIndex].ToString();
             string b = list[nm];
             player.Open(new Uri(b, UriKind.Relative));
-            time = player.NaturalDuration;
+            //time = player.NaturalDuration;
         }
 
         private void Pause_Click(object sender, RoutedEventArgs e)
         {
             player.Pause();
+            
         }
 
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
             player.Stop();
+            Timer.Stop();
             dlina.Content = "";
+            tv.Content = "";
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-           
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.ShowDialog();
-            
-            nm = System.IO.Path.GetFileNameWithoutExtension(dlg.FileName);
-            list.Add(nm, dlg.FileName);
-            stack.Items.Add(nm);
 
+            //Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //dlg.ShowDialog();
+            //InitializeOpenFileDialog();
+
+          
+            //DialogResult dr = this.dlg.ShowDialog();
+            //if (dr == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    // Read the files
+            //    foreach (String file in dlg.FileNames)
+            //    {
+            //        // Create a PictureBox.
+            //        try
+            //        {
+                        nm = System.IO.Path.GetFileNameWithoutExtension(dlg.FileName);
+                        list.Add(nm, dlg.FileName);
+                        stack.Items.Add(nm);
+            //        }
+            //        catch (SecurityException ex)
+            //        {
+            //            // The user lacks appropriate permissions to read files, discover paths, etc.
+            //            MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+            //                "Error message: " + ex.Message + "\n\n" +
+            //                "Details (send to Support):\n\n" + ex.StackTrace
+            //            );
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            // Could not load the image - probably related to Windows file system permissions.
+            //            MessageBox.Show("Cannot display the image: " + file.Substring(file.LastIndexOf('\\'))
+            //                + ". You may not have permission to read the file, or " +
+            //                "it may be corrupt.\n\nReported error: " + ex.Message);
+            //        }
+            //    }
+
+            //}
         }
 
         private void volume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            int sliderValue = (int)volume.Value;
+        }
 
+        private void slid_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            daHotKak = true;
+        }
+
+        private void slid_DragEnded(object sender, DragCompletedEventArgs e)
+        {
+            int sliderValue = (int)slid.Value;
+
+            ts = new TimeSpan(0, 0, sliderValue);
+            player.Position = ts;
+            daHotKak = false;
         }
 
         private void slid_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             int sliderValue = (int)slid.Value;
 
-            ts = new TimeSpan(0, 0, 0, 0, sliderValue);
-            player.Position = ts;
+            tv.Content = (sliderValue / 3600).ToString() + ":" + (sliderValue / 60).ToString() + ":" + (sliderValue % 60).ToString();
         }
-
     }
 }
---
+++ MP4/MP4/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MP4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer Timer;
+        TimeSpan ts;
+        Duration time;
+        bool v = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Timer = new System.Windows.Threading.DispatcherTimer();
+            Timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (v == false)
+            {
+                tv.Content = vid.Position.Hours + ":" + vid.Position.Minutes + ":" + vid.Position.Seconds;
+                slid.Value = vid.Position.TotalSeconds;
+            }
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+
+            vid.Source = new Uri(dlg.FileName, UriKind.Relative);
+
+            time = vid.NaturalDuration;
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            vid.Play();
+            Timer.Start();
+            dlina.Content = time.TimeSpan.Hours + ":" + time.TimeSpan.Minutes + ":" + time.TimeSpan.Seconds;
+            tv.Content = "0:0:0";
+        }
+
+        private void pause_Click(object sender, RoutedEventArgs e)
+        {
+            vid.Pause();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            vid.Stop();
+            dlina.Content = "";
+            tv.Content = "";
+        }
+
+        private void vid_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            slid.Maximum = vid.NaturalDuration.TimeSpan.TotalSeconds;
+            slid.Value = 0;
+        }
+
+        private void slid_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int sliderValue = (int)slid.Value;
+
+            tv.Content = (sliderValue / 3600).ToString() + ":" + (sliderValue / 60).ToString() + ":" + (sliderValue % 60).ToString();
+        }
+
+        private void slid_ValueStarted(object sender, DragStartedEventArgs e)
+        {
+            v = true;
+        }
+
+        private void slid_ValueCompleted(object sender, DragCompletedEventArgs e)
+        {
+            int sliderValue = (int)slid.Value;
+
+            ts = new TimeSpan(0, 0, sliderValue);
+            vid.Position = ts;
+            v = false;
+        }
+    }
+}
