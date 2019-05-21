a377f28449fb4d13bf12b11402dcd48a4d40ad79
Sidsand <dshordg@gmail.com>
2019-05-21 13:17:39 +0700

    with love

--- MP3/MP3/MainWindow.xaml.cs
+++ MP3/MP3/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace MP3
         Dictionary<string, string> list = new Dictionary<string, string>();
         MediaPlayer player = new MediaPlayer();
         TimeSpan ts;
-        int n = -1;
+        int n = -1, q;
         string nm;
         Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
         bool daHotKak = false;
@@ -53,8 +53,9 @@ namespace MP3
             player.Volume = vol.Value/100.0;
             slid.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
             slid.Value = 0;
-            dlina.Content = player.NaturalDuration.TimeSpan.Hours + ":" + player.NaturalDuration.TimeSpan.Minutes + ":" + player.NaturalDuration.TimeSpan.Seconds;
             tv.Content = "0:0:0";
+            dlina.Content = player.NaturalDuration.TimeSpan.Hours + ":" + player.NaturalDuration.TimeSpan.Minutes + ":" + player.NaturalDuration.TimeSpan.Seconds;
+
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
@@ -75,16 +76,29 @@ namespace MP3
                             player.Open(new Uri(b, UriKind.Relative));
                             player.Play();
                             Timer.Start();
+                            tv.Content = "0:0:0";
+                            
                         }
+                        else
+                        {
+                            Timer.Stop();
+                            player.Stop();
+                        }                           
                     }
                     else
                     {
-                        stack.SelectedIndex = new Random().Next(0, n);
-                        nm = stack.Items[stack.SelectedIndex].ToString();
-                        string b = list[nm];
-                        player.Open(new Uri(b, UriKind.Relative));
-                        player.Play();
-                        Timer.Start();
+                        while (q == stack.SelectedIndex)
+                        {
+                            q = new Random().Next(0, n);
+                        }
+                        stack.SelectedIndex = q;
+                            nm = stack.Items[stack.SelectedIndex].ToString();
+                            string b = list[nm];
+                            player.Open(new Uri(b, UriKind.Relative));
+                            player.Play();
+                            Timer.Start();
+                            tv.Content = "0:0:0";
+                        
                     }
                 }
             }
@@ -92,20 +106,27 @@ namespace MP3
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            if (rand.IsChecked == true)
+            try
             {
-                stack.SelectedIndex = new Random().Next(0, n);
+                player.Play();
+                Timer.Start();
+                tv.Content = "0:0:0";
+                dlina.Content = player.NaturalDuration.TimeSpan.Hours + ":" + player.NaturalDuration.TimeSpan.Minutes + ":" + player.NaturalDuration.TimeSpan.Seconds;
+            }
+            catch 
+            {
+                q = new Random().Next(0, n);
+
+                stack.SelectedIndex = q;
                 nm = stack.Items[stack.SelectedIndex].ToString();
                 string b = list[nm];
                 player.Open(new Uri(b, UriKind.Relative));
                 player.Play();
                 Timer.Start();
+                tv.Content = "0:0:0";
+                
             }
-            else
-            {
-                player.Play();
-                Timer.Start();
-            }
+           
         }
 
         private void Stack_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -131,13 +152,22 @@ namespace MP3
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            n++;
+            
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.ShowDialog();
-            nm = System.IO.Path.GetFileNameWithoutExtension(dlg.FileName);
-            list.Add(nm, dlg.FileName);
+            dlg.Multiselect = true;
 
-            stack.Items.Add(nm);
+            dlg.ShowDialog();
+
+            foreach (string s in dlg.FileNames)
+            {
+                n++;
+                //nm = System.IO.Path.GetFileNameWithoutExtension(dlg.FileName);
+
+                nm = System.IO.Path.GetFileNameWithoutExtension(s);
+                list.Add(nm, s);
+
+                stack.Items.Add(nm);
+            }
         }
 
         private void volume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
